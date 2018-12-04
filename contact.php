1d273eef6b20bb928079b0bdd47c18adb919b517
Bia McCrory <bia@Bias-MacBook-Pro.local>
2018-12-04 15:08:43 -0800

    first commit

---
+++ contact.php
@@ -0,0 +1,36 @@
+<?php
+if ($_POST["submit"]) {
+	if (!$_POST['name']) {
+		$error = "<br />Please enter your name";
+	}
+	if (!$_POST['email']) {
+		$error .= "<br />Please enter your email address";
+	}
+	if (!$_POST['phone']) {
+		$error .= "<br /> Please enter your phone number";
+	}
+	if (!$_POST['message']) {
+		$error .= "<br />Please enter a message";
+	}
+	if ($_POST['email'] != "" and !filter_var(
+		$_POST['email'],
+		FILTER_VALIDATE_EMAIL
+	)) {
+		$error .= "<br />Please enter a valid email address";
+	}
+	if ($error) {
+		$result = '<div class="alert alert-danger"><strong>There were error(s) in your form:</strong>' . $error . '</div>';
+	} else {
+ /* THE EMAIL WHERE YOU WANT TO RECEIVE THE CONTACT MESSAGES */
+		if (mail(
+			"info@orangecirclecompany.com",
+			"Contact Message from Deck Installation",
+			"Name: " . $_POST['name'] . "Email: " . $_POST['email'] . "Phone: " . $_POST['phone'] . "Message: " . $_POST['message']
+		)) {
+			$result = '<div class="alert alert-success"> <strong> Thank you!</strong> We\'ll get back to you shortly.</div>';
+		} else {
+			$result = '<div class="alert alert-danger">Sorry, there was an error sending your message. Please try again later.</div>';
+		}
+	}
+}
+?>
